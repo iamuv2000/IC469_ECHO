10139e2a37753797198bfc0fe302161508cb7c8e
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-06-28 16:49:30 +0530

    Updated eslint configs and implemented code style
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const app = require('express')();
-const logger = require('./controllers/logger.js');
-require('./configs/mongobd/mongodb-config');
+const app = require('express')()
+const logger = require('./controllers/logger.js')
+require('./configs/mongobd/mongodb-config')
 
 app.listen(8080, () => {
-    logger.server('Server listening: 8080');
+  logger.server('Server listening: 8080')
 })
