b7d6f1cb5f1d62b6f1c2e8c10daaecfa2284d3bb
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-08-01 19:51:01 +0530

    Updated activity routes
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- controllers/Activity/FetchDailyActivity.js
+++ controllers/Activity/FetchDailyActivity.js
@@ -1,15 +1,33 @@
 const responses = require('../../configs/responses.js')
+const Activity = require('../../models/Activity.js')
 
-const FetchDailyActivity = () => {
+const FetchDailyActivity = (uid) => {
     return new Promise((resolve, reject) => {
-        resolve({
-            statusCode: 200,
-            serverMessage: responses['200'],
-            payload: {
-                activities: ["jogging", "meditate", "code", "netflix", "paint"]
-            },
-            error: null
-        })
+        Activity.findOne({ uid })
+            .then((activityInstance) => activityInstance.toObject())
+            .then((activity) => {
+                console.log(activity)
+                if (activity.activityRecords.length < 4) {
+                    return resolve({
+                        statusCode: 200,
+                        serverMessage: responses['200'],
+                        payload: {
+                            activities: [],
+                            hasEnoughRecords: false,
+                        },
+                        error: null
+                    })
+                }
+                resolve({
+                    statusCode: 200,
+                    serverMessage: responses['200'],
+                    payload: {
+                        activities: ["jogging", "meditate", "code", "netflix", "paint"],
+                        hasEnoughRecords: true,
+                    },
+                    error: null
+                })
+            })
     })
 }
 
--- routes/activity/DailyActivities.js
+++ routes/activity/DailyActivities.js
@@ -2,7 +2,7 @@ const route = require("express")();
 const FetchDailyActivity = require('../../controllers/Activity/FetchDailyActivity.js')
 
 route.get('/user/activity/daily', (req, res) => {
-    FetchDailyActivity()
+    FetchDailyActivity(req.uid)
         .then((resp) => res.status(resp.statusCode).send(resp))
         .catch((err) => res.status(err.statusCode).send(err))
 })
