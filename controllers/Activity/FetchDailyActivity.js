bfcaa99595d171141d3159b235b2e6e98745095a
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-08-03 13:50:39 +0530

    Fixes the chaining resume problem on suggested activity route
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- controllers/Activity/FetchDailyActivity.js
+++ controllers/Activity/FetchDailyActivity.js
@@ -8,7 +8,6 @@ const FetchDailyActivity = (uid) => {
     Activity.findOne({ uid })
       .then((activityInstance) => activityInstance.toObject())
       .then((activity) => {
-        // console.log(activity)
         const lengthOfRecords = activity.activityRecords.length
         if (lengthOfRecords < 5) {
           return resolve({
@@ -20,45 +19,55 @@ const FetchDailyActivity = (uid) => {
             },
             error: null
           })
-        }
-        var alteredArray = []
-        const startIndex = lengthOfRecords > 15 ? lengthOfRecords - 16 : 0
-        const endIndex = lengthOfRecords
-        activity.activityRecords.slice(startIndex, endIndex).forEach(async (record) => {
-          var vals = []
-          record.forEach((value, key, map) => {
-            vals.push(value)
+        } else {
+          var alteredArray = []
+          const startIndex = lengthOfRecords > 15 ? lengthOfRecords - 16 : 0
+          const endIndex = lengthOfRecords
+          activity.activityRecords.slice(startIndex, endIndex).forEach(async (record) => {
+            var vals = []
+            record.forEach((value, key, map) => {
+              vals.push(value)
+            })
+            return alteredArray.push(vals)
           })
-          return alteredArray.push(vals)
-        })
-        const body = JSON.stringify({
-          indexKeys: activity.userActivities,
-          record: alteredArray
-        })
-        return fetch('https://echo-cbt-micro.herokuapp.com/activity-suggestions', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body
-        })
-      })
-      .then((r) => r.json())
-      .then((response) => {
-        const respKeys = Object.keys(response)
-        respKeys.sort((a, b) => {
-          return response[b] - response[a]
-        })
-        resolve({
-          statusCode: 200,
-          serverMessage: responses['200'],
-          payload: {
-            activities: respKeys.slice(0, 5),
-            hasEnoughRecords: true
-          },
-          error: null
-        })
+          const body = JSON.stringify({
+            indexKeys: activity.userActivities,
+            record: alteredArray
+          })
+          fetch('https://echo-cbt-micro.herokuapp.com/activity-suggestions', {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body
+          })
+            .then((r) => r.json())
+            .then((response) => {
+              const respKeys = Object.keys(response)
+              respKeys.sort((a, b) => {
+                return response[b] - response[a]
+              })
+              resolve({
+                statusCode: 200,
+                serverMessage: responses['200'],
+                payload: {
+                  activities: respKeys.slice(0, 5),
+                  hasEnoughRecords: true
+                },
+                error: null
+              })
+            })
+            .catch((err) => {
+              console.log(err.message)
+              reject({
+                statusCode: 500,
+                serverMessage: responses['500'],
+                payload: {},
+                error: 'Server Side Error'
+              })
+            })
+        }
       })
       .catch((err) => {
         console.log(err.message)
