461bd38d402703b102fb8dcf37f508b88e1cf28d
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-08-03 13:29:11 +0530

    Added log to debug error
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- routes/activity/DailyActivities.js
+++ routes/activity/DailyActivities.js
@@ -1,10 +1,11 @@
-const route = require("express")();
+const route = require('express')()
 const FetchDailyActivity = require('../../controllers/Activity/FetchDailyActivity.js')
 
 route.get('/user/activity/daily', (req, res) => {
-    FetchDailyActivity(req.uid)
-        .then((resp) => res.status(resp.statusCode).send(resp))
-        .catch((err) => res.status(err.statusCode).send(err))
+  console.log(req.uid)
+  FetchDailyActivity(req.uid)
+    .then((resp) => res.status(resp.statusCode).send(resp))
+    .catch((err) => res.status(err.statusCode).send(err))
 })
 
-module.exports = route;
+module.exports = route
