5f42000166285615b4128b8e4633f52e2002df06
Yuvraj Singh <yuvraj.singh2018@vitstudent.ac.in>
2020-08-03 14:01:01 +0530

    Integrated profile stories

--- Flutter App/lib/screens/profilePage.dart
+++ Flutter App/lib/screens/profilePage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-
 import 'addMentor.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../widgets/myStoryCard.dart';
+FirebaseUser loggedInUser;
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -10,14 +14,50 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage>
     with SingleTickerProviderStateMixin {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final _auth = FirebaseAuth.instance;
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.displayName);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<http.Response> getMyPosts() {
+    return http.get('https://echo-cbt-server.herokuapp.com/user/getUserStories',
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ${loggedInUser.uid}'
+        });
+  }
 
   TabController _tabController;
+
+  var posts = [];
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    getCurrentUser();
+    _initializePage();
   }
+
+  void _initializePage() async {
+    print("FETCHING ARTICLES!");
+    var allPosts = await getMyPosts();
+    final parsed = jsonDecode(allPosts.body);
+    print(parsed['payload']);
+//    var a = await model.getAllPosts();
+    setState(() {
+      posts = parsed['payload']['userStories'];
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -78,6 +118,9 @@ class _ProfilePageState extends State<ProfilePage>
               Container(
                 margin: EdgeInsets.only(top: 50),
                 child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    loggedInUser.photoUrl,
+                  ),
                   backgroundColor: Colors.grey,
                   radius: 60.0,
                 ),
@@ -89,7 +132,7 @@ class _ProfilePageState extends State<ProfilePage>
             children: <Widget>[
               Container(
                 child: Text(
-                  'Gagan Verma',
+                  loggedInUser.displayName,
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 18,
@@ -103,7 +146,7 @@ class _ProfilePageState extends State<ProfilePage>
             children: <Widget>[
               Container(
                 child: Text(
-                  'gagan.v@gmail.com',
+                  loggedInUser.email,
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 14,
@@ -137,83 +180,27 @@ class _ProfilePageState extends State<ProfilePage>
               child: TabBarView(
                 controller: _tabController,
                 children: [
-                  ListView(
-                    padding: const EdgeInsets.all(8),
-                    children: <Widget>[
-                      Card(
-                        elevation: 8.0,
-                        child: Column(
-                          children: <Widget>[
-                            ListTile(
-                              leading: CircleAvatar(
-                                radius: 16,
-                              ),
-                              title: Text('Posted Anonymously'),
-                              subtitle: Text('A month ago.'),
-                              trailing: Icon(Icons.more_vert),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                child: Text(
-                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non enim nulla turpis id. Id sed tellus, velit laoreet amet ipsum netus. Sed proin duis et leo, nam vitae porta. Enim, lorem curabitur id facilisis nisi, pulvinar magnis cursus neque. Ut ipsum et, felis nam euismod. ',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                          ],
+                  posts.length == 0
+                  ?
+                      Center(
+                        child: Text(
+                          'No stories to display!',
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500
+                          ),
                         ),
-                      ),
-                      Card(
-                        elevation: 8.0,
-                        child: Column(
-                          children: <Widget>[
-                            ListTile(
-                              leading: CircleAvatar(
-                                radius: 16,
-                              ),
-                              title: Text('Posted Anonymously'),
-                              subtitle: Text('A month ago.'),
-                              trailing: Icon(Icons.more_vert),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                child: Text(
-                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non enim nulla turpis id. Id sed tellus, velit laoreet amet ipsum netus. Sed proin duis et leo, nam vitae porta. Enim, lorem curabitur id facilisis nisi, pulvinar magnis cursus neque. Ut ipsum et, felis nam euismod. ',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Card(
-                        elevation: 8.0,
-                        child: Column(
-                          children: <Widget>[
-                            ListTile(
-                              leading: CircleAvatar(
-                                radius: 16,
-                              ),
-                              title: Text('Posted Anonymously'),
-                              subtitle: Text('A month ago.'),
-                              trailing: Icon(Icons.more_vert),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                child: Text(
-                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non enim nulla turpis id. Id sed tellus, velit laoreet amet ipsum netus. Sed proin duis et leo, nam vitae porta. Enim, lorem curabitur id facilisis nisi, pulvinar magnis cursus neque. Ut ipsum et, felis nam euismod. ',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                      )
+                      :
+                  ListView.builder(
+                      itemCount: posts.length,
+                      padding: EdgeInsets.all(0),
+                      itemBuilder: (context, index) {
+                        final item = posts[index];
+                        return Container(
+                          child: MessageCard(message: item),
+                        );
+                      }),
                   ListView(
                     padding: const EdgeInsets.all(8),
                     children: <Widget>[
---
+++ Flutter App/lib/widgets/myStoryCard.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import '../screens/commentsPage.dart';
+import 'package:random_color/random_color.dart';
+
+
+
+class MessageCard extends StatefulWidget {
+
+  var message;
+  MessageCard({this.message});
+
+  @override
+  _MessageCardState createState() => _MessageCardState();
+}
+
+class _MessageCardState extends State<MessageCard> {
+
+  static RandomColor randomColor = RandomColor();
+  Color _color = randomColor.randomColor(
+      colorSaturation: ColorSaturation.highSaturation,
+      colorHue: ColorHue.orange
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) =>  CommentsPage(postId : widget.message["_id"])));
+      },
+      child: Card(
+        // color: Colors.black,
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15)
+        ),
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+        // shape: ,
+        child: Container(
+          // margin: EdgeInsets.all(10),
+          color: Colors.white,
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 15, left: 10, right: 10, top: 10),
+                  child: Wrap(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundColor: _color,
+                            child: Text(
+                                widget.message['name'][0],
+                              style: TextStyle(
+                                color: Colors.white
+                              )
+                            ),
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                widget.message['name'],
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600
+                                ),
+                              ),
+//                              Container(
+//                                margin: EdgeInsets.only(left: 25),
+//                                child: Text(
+//                                    difference.toString()
+//                                ),
+//                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(left: 10),
+                            child: Text(
+                              widget.message['story'],
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
